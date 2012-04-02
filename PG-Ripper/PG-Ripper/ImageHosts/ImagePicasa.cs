5d8f4770be4484af23c9caeaa056e82d9f1e7371
Watcher <Watcher@Watcher-One>
2012-04-02 13:14:59 +0200

    - NEW: PG-Ripper now Supports different Forum Accounts from different sites, you can switch between forums from the menu - NEW: Added Support for "DirectUpload.net" links - NEW: Added Support for "PixRoute.com" links - NEW: Added Support for "ImagePicasa.com" links - NEW: Added Support for "PixHub.eu" links - FIXED: "Hoooster.com" links

---
+++ PG-Ripper/PG-Ripper/ImageHosts/ImagePicasa.cs
@@ -0,0 +1,171 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ImagePicasa.cs" company="The Watcher">
+//   Copyright (c) The Watcher Partial Rights Reserved.
+//  This software is licensed under the MIT license. See license.txt for details.
+// </copyright>
+// <summary>
+//   Code Named: PG-Ripper
+//   Function  : Extracts Images posted on VB forums and attempts to fetch them to disk.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace PGRipper.ImageHosts
+{
+    using System;
+    using System.Collections;
+    using System.IO;
+    using System.Net;
+    using System.Text.RegularExpressions;
+    using System.Threading;
+
+    using PGRipper.Objects;
+
+    /// <summary>
+    /// Worker class to get images from ImagePicasa.com
+    /// </summary>
+    public class ImagePicasa : ServiceTemplate
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ImagePicasa"/> class.
+        /// </summary>
+        /// <param name="savePath">
+        /// The save Path.
+        /// </param>
+        /// <param name="imageUrl">
+        /// The image Url.
+        /// </param>
+        /// <param name="hashtable">
+        /// The hashtable.
+        /// </param>
+        public ImagePicasa(ref string savePath, ref string imageUrl, ref Hashtable hashtable)
+            : base(savePath, imageUrl, ref hashtable)
+        {
+        }
+
+        /// <summary>
+        /// Do the Download
+        /// </summary>
+        /// <returns>
+        /// Returns if the Image was downloaded
+        /// </returns>
+        protected override bool DoDownload()
+        {
+            string strImgURL = mstrURL;
+
+            if (eventTable.ContainsKey(strImgURL))
+            {
+                return true;
+            }
+
+            var filePath = string.Empty;
+
+            try
+            {
+                if (!Directory.Exists(mSavePath))
+                {
+                    Directory.CreateDirectory(mSavePath);
+                }
+            }
+            catch (IOException ex)
+            {
+                MainForm.DeleteMessage = ex.Message;
+                MainForm.Delete = true;
+
+                return false;
+            }
+
+            CacheObject ccObj = new CacheObject { IsDownloaded = false, FilePath = filePath, Url = strImgURL };
+
+            try
+            {
+                eventTable.Add(strImgURL, ccObj);
+            }
+            catch (ThreadAbortException)
+            {
+                return true;
+            }
+            catch (Exception)
+            {
+                if (eventTable.ContainsKey(strImgURL))
+                {
+                    return false;
+                }
+
+                eventTable.Add(strImgURL, ccObj);
+            }
+
+            string sPage = GetImageHostPage(ref strImgURL);
+
+            if (sPage.Length < 10)
+            {
+                return false;
+            }
+
+            string strNewURL;
+
+            var m = Regex.Match(sPage, @"popitup\('(?<inner>[^\""]*)'", RegexOptions.Singleline);
+
+            if (m.Success)
+            {
+                strNewURL = m.Groups["inner"].Value;
+            }
+            else
+            {
+                return false;
+            }
+
+            filePath = strNewURL.Substring(strNewURL.LastIndexOf("/", StringComparison.Ordinal) + 1);
+
+            filePath = Path.Combine(mSavePath, Utility.RemoveIllegalCharecters(filePath));
+
+            //////////////////////////////////////////////////////////////////////////
+
+            string newAlteredPath = Utility.GetSuitableName(filePath);
+            if (filePath != newAlteredPath)
+            {
+                filePath = newAlteredPath;
+                ((CacheObject)eventTable[mstrURL]).FilePath = filePath;
+            }
+
+            try
+            {
+                WebClient client = new WebClient();
+                client.Headers.Add(string.Format("Referer: {0}", strImgURL));
+                client.Headers.Add("User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US; rv:1.7.10) Gecko/20050716 Firefox/1.0.6");
+                client.DownloadFile(strNewURL, filePath);
+                client.Dispose();
+            }
+            catch (ThreadAbortException)
+            {
+                ((CacheObject)eventTable[strImgURL]).IsDownloaded = false;
+                ThreadManager.GetInstance().RemoveThreadbyId(mstrURL);
+
+                return true;
+            }
+            catch (IOException ex)
+            {
+                MainForm.DeleteMessage = ex.Message;
+                MainForm.Delete = true;
+
+                ((CacheObject)eventTable[strImgURL]).IsDownloaded = false;
+                ThreadManager.GetInstance().RemoveThreadbyId(mstrURL);
+
+                return true;
+            }
+            catch (WebException)
+            {
+                ((CacheObject)eventTable[strImgURL]).IsDownloaded = false;
+                ThreadManager.GetInstance().RemoveThreadbyId(mstrURL);
+
+                return false;
+            }
+
+            ((CacheObject)eventTable[mstrURL]).IsDownloaded = true;
+            CacheController.GetInstance().uSLastPic = ((CacheObject)eventTable[mstrURL]).FilePath = filePath;
+
+            return true;
+        }
+
+        //////////////////////////////////////////////////////////////////////////
+    }
+}
---
+++ PG-Ripper/PG-Ripper/Objects/ForumAccount.cs
@@ -0,0 +1,47 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ForumAccount.cs" company="The Watcher">
+//   Copyright (c) The Watcher Partial Rights Reserved.
+//  This software is licensed under the MIT license. See license.txt for details.
+// </copyright>
+// <summary>
+//   Code Named: PG-Ripper
+//   Function  : Extracts Images posted on RiP forums and attempts to fetch them to disk.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace PGRipper.Objects
+{
+    /// <summary>
+    /// The Forum Account.
+    /// </summary>
+    public class ForumAccount : object
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the forum URL.
+        /// </summary>
+        /// <value>
+        /// The forum URL.
+        /// </value>
+        public string ForumURL { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the user.
+        /// </summary>
+        /// <value>
+        /// The name of the user.
+        /// </value>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user pass word.
+        /// </summary>
+        /// <value>
+        /// The user pass word.
+        /// </value>
+        public string UserPassWord { get; set; }
+
+        #endregion
+    }
+}
