434b1f09f0879edfd976cb1950e14dd3dc24c394
Ingo <w8tcha@users.noreply.github.com>
2013-07-27 10:19:17 +0200

    fixed login

--- VG-Ripper/VG-Ripper/LoginManager.cs
+++ VG-Ripper/VG-Ripper/LoginManager.cs
@@ -17,7 +17,7 @@ namespace RiPRipper
     using System.Windows.Forms;
 
     /// <summary>
-    /// The hot sauce for Login management onto the RiP Forums.
+    /// The hot sauce for Login management onto the Forums.
     /// Sends basic login credentials and if successfull, commits 
     /// cookies to hashtable.
     /// If operation successfull, true is returned, otherwise false.
@@ -58,9 +58,6 @@ namespace RiPRipper
             var postData =
                 "do=login&forceredirect=1&url=%2Fforum%2F&vb_login_md5password=%md5pass%&vb_login_md5password_utf=%md5pass%&s=&vb_login_username=%md5user%&vb_login_password=&cookieuser=1";
 
-            ////string hashedPassword = Utility.EncodePassword(sPass).Replace("-", "").ToLower();
-
-            // sPostData = sPostData.Replace("%md5pass%", hashedPassword);
             postData = postData.Replace("%md5pass%", this.sPass);
             postData = postData.Replace("%md5user%", this.sUsr);
 
@@ -72,8 +69,6 @@ namespace RiPRipper
 
                 var req = (HttpWebRequest)WebRequest.Create(loginURL);
 
-                req.UserAgent =
-                    "Mozilla/5.0 (Windows NT 6.1; rv:15.0) Gecko/20120716 Firefox/15.0";
                 req.CookieContainer = cookieContainer;
                 req.Method = "POST";
                 req.ContentType = "application/x-www-form-urlencoded";
@@ -105,7 +100,7 @@ namespace RiPRipper
                         loginSuccessfully = true;
                     }
                 }
-            }
+           }
             catch (Exception ex)
             {
                 MessageBox.Show(string.Format("{0}\n{1}", ex.Message, ex.StackTrace));
