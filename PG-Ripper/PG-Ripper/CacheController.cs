ecf736f4c5ff3d57190ac964115a05dfc45b3824
tha_watcha <tha_watcha@localhost>
2013-08-18 08:20:42 +0200

    pg-ripper NEW: Added Support for "PicsLite.com" links
    NEW: Added Support for "PictureDip.com" links
    
    FIXED: "ImgBabes.com" links

--- PG-Ripper/PG-Ripper/CacheController.cs
+++ PG-Ripper/PG-Ripper/CacheController.cs
@@ -620,6 +620,10 @@ namespace PGRipper
             {
                 lThreadStart = imageDownloader.GetImgWoot;
             }
+            else if (imageUrl.IndexOf(@"picslite.com/") >= 0)
+            {
+                lThreadStart = imageDownloader.GetImgWoot;
+            }
             else if (imageUrl.IndexOf(@"imagefolks.com/") >= 0 || imageUrl.IndexOf(@"ImageFolks.com/") >= 0)
             {
                 lThreadStart = imageDownloader.GetImageFolks;
@@ -675,6 +679,10 @@ namespace PGRipper
             else if (imageUrl.IndexOf(@"imagesion.com/") >= 0)
             {
                 lThreadStart = imageDownloader.GetImagesIon;
+            }
+            else if (imageUrl.IndexOf(@"picturedip.com/") >= 0)
+            {
+                lThreadStart = imageDownloader.GetPictureDip;
             }
             else if (imageUrl.IndexOf("ayhja.com/") >= 0)
             {
--- PG-Ripper/PG-Ripper/Properties/AssemblyInfo.cs
+++ PG-Ripper/PG-Ripper/Properties/AssemblyInfo.cs
@@ -31,8 +31,8 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.4.1.6")]
-[assembly: AssemblyFileVersionAttribute("1.4.1.6")]
+[assembly: AssemblyVersion("1.4.1.7")]
+[assembly: AssemblyFileVersionAttribute("1.4.1.7")]
 
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyName("")]
