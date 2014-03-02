e53cb1bdeb475ad544fd344581c20ef0e9ae25f8
tha_watcha <tha_watcha@localhost>
2014-03-02 10:51:07 +0100

    bug fixes

--- VG-Ripper/Properties/AssemblyInfo.cs
+++ VG-Ripper/Properties/AssemblyInfo.cs
@@ -31,8 +31,8 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.9.5.6")]
-[assembly: AssemblyFileVersionAttribute("2.9.5.6")]
+[assembly: AssemblyVersion("2.9.5.7")]
+[assembly: AssemblyFileVersionAttribute("2.9.5.7")]
 
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyName("")]
